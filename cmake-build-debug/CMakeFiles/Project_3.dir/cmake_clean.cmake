386661aa7078220a7e5b207604ef809e8feb2b76
Dionysis <dionysisath@gmail.com>
2021-01-18 12:17:58 +0200

    fix

--- cmake-build-debug/CMakeFiles/Project_3.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/Project_3.dir/cmake_clean.cmake
@@ -1,5 +1,7 @@
 file(REMOVE_RECURSE
+  "CMakeFiles/Project_3.dir/Job.cpp.o"
   "CMakeFiles/Project_3.dir/JobScheduler.cpp.o"
+  "CMakeFiles/Project_3.dir/Queue.cpp.o"
   "CMakeFiles/Project_3.dir/main.cpp.o"
   "Project_3"
   "Project_3.pdb"

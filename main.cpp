e47e029a176c7a711edfad433fece66efd9ef89b
treloubas76 <chris96skan@gmail.com>
2021-01-18 11:14:59 +0200

    all project files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <cstdlib>
+#include <thread>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+
+void foo()
+{
+    while (true) {
+        cout << "yo" << endl;
+    }
+}
+
+void bar()
+{
+    while (true) {
+        cout << "men" << endl;
+    }
+}
+
+int main()
+{
+    std::thread first (foo);     // spawn new thread that calls foo()
+    std::thread second (bar);  // spawn new thread that calls bar(0)
+
+
+
+    // synchronize threads:
+    first.join();                // pauses until first finishes
+    second.join();               // pauses until second finishes
+
+    std::cout << "foo and bar completed.\n";
+
     return 0;
 }
